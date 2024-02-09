67cee547e6f76a553e6eacc5d29dfa8f02dedf8d
LucaFrisoni <frisoniluca1@gmail.com>
2024-02-09 14:24:05 -0300

    Feat update reactDom

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,31 +7,23 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 const headingFont = localFont({
-  src: "../../public/fonts/font.woff2"
+  src: "../../public/fonts/font.woff2",
 });
 
 const textFont = Poppins({
   subsets: ["latin"],
-  weight: [
-    "100",
-    "200",
-    "300",
-    "400",
-    "500",
-    "600",
-    "700",
-    "800",
-    "900"
-  ],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className={cn(
-        "flex items-center justify-center flex-col",
-        headingFont.className,
-      )}>
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
@@ -43,16 +35,18 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className={cn(
-        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
-        textFont.className,
-      )}>
-        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className
+        )}
+      >
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is unique -
+        accomplish it all with Taskify.
       </div>
       <Button className="mt-6" size="lg" asChild>
-        <Link href="/sign-up">
-          Get Taskify for free
-        </Link>
+        <Link href="/sign-up">Get Taskify for free</Link>
       </Button>
     </div>
   );
