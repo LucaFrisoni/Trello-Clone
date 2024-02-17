1450686c6051dde8ebfb98e56dd0cd751e04665d
LucaFrisoni <frisoniluca1@gmail.com>
2024-02-16 22:15:25 -0300

    Feat:Change with useEffect-Suspense

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -20,12 +20,24 @@ export const Info = ({ isPro }: InfoProps) => {
   }, []);
 
   if (!isMounted) {
-    return null;
+    return (
+      <div className="flex items-center gap-x-4">
+        <div className="w-[60px] h-[60px] relative">
+          <Skeleton className="w-full h-full absolute" />
+        </div>
+        <div className="space-y-2">
+          <Skeleton className="h-10 w-[200px]" />
+          <div className="flex items-center">
+            <Skeleton className="h-4 w-4 mr-2" />
+            <Skeleton className="h-4 w-[100px]" />
+          </div>
+        </div>
+      </div>
+    );
   }
 
-  if (!isLoaded) {
-    return <Info.Skeleton />;
-  }
+  // if (!isLoaded) {
+  // }
 
   return (
     <div className="flex items-center gap-x-4">
@@ -48,19 +60,8 @@ export const Info = ({ isPro }: InfoProps) => {
   );
 };
 
-Info.Skeleton = function SkeletonInfo() {
-  return (
-    <div className="flex items-center gap-x-4">
-      <div className="w-[60px] h-[60px] relative">
-        <Skeleton className="w-full h-full absolute" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="h-10 w-[200px]" />
-        <div className="flex items-center">
-          <Skeleton className="h-4 w-4 mr-2" />
-          <Skeleton className="h-4 w-[100px]" />
-        </div>
-      </div>
-    </div>
-  );
-};
+// Info.Skeleton = function SkeletonInfo() {
+//   return (
+
+//   );
+// };
