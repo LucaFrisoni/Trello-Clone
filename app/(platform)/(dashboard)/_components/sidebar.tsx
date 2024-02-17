89b12d627a73b4660e027cb6be0c22529c10ddd7
LucaFrisoni <frisoniluca1@gmail.com>
2024-02-16 21:22:13 -0300

    Feat: incorporing Suspense

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -6,11 +6,11 @@ import { useLocalStorage } from "usehooks-ts";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 
 import { NavItem, Organization } from "./nav-item";
+import { Suspense } from "react";
 
 interface SidebarProps {
   storageKey?: string;
@@ -55,7 +55,7 @@ export const Sidebar = ({
 
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
-      <>
+      <Suspense>
         <div className="flex items-center justify-between mb-2">
           <Skeleton className="h-10 w-[50%]" />
           <Skeleton className="h-10 w-10" />
@@ -65,7 +65,7 @@ export const Sidebar = ({
           <NavItem.Skeleton />
           <NavItem.Skeleton />
         </div>
-      </>
+      </Suspense>
     );
   }
 
